7bc6ded301e079a79774cf408653d930c049e383
JPyke3 <github@pyk.ee>
2024-07-29 14:00:13 +1000

    Removed Tmux Navigator

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -126,23 +126,4 @@ return {
 
     -- Other Plugins
     'vimpostor/vim-tpipeline',
-
-    -- Tmux Navigator
-    {
-        cmd = {
-            "TmuxNavigateLeft",
-            "TmuxNavigateDown",
-            "TmuxNavigateUp",
-            "TmuxNavigateRight",
-            "TmuxNavigatePrevious",
-        },
-        keys = {
-            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-        },
-        lazy = true,
-    },
 }
